12d5533a9afb8859e6f707f2074fefa7054d759a
MaximilianoPeña <maxitofinn@gmail.com>
2025-02-26 23:16:45 -0600

    Adicion de los 2 archivos para cahllenge
    En este commit realice todos los cambios para terminar el programa del
    desafio de amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let arregloNombres = [];
+
+function agregarAmigo(){
+    let boxNombres = document.getElementById("amigo").value;
+    let texto = document.getElementById("listaAmigos");
+    if(document.getElementById("amigo").value==""){
+        alert("Porfavor ingresa algun nombre");
+    }else{
+        arregloNombres.push(boxNombres);
+        texto.innerHTML = arregloNombres;
+        document.getElementById("amigo").value="";
+    }
+    
+}
+
+function sortearAmigo(){
+   document.getElementById("listaAmigos").innerHTML="";
+    let numeroAleatorio = Math.floor(Math.random()*arregloNombres.length);
+    console.log(numeroAleatorio);
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = "El amigo secreto sorteado es:" + arregloNombres[numeroAleatorio];
+    
+}
